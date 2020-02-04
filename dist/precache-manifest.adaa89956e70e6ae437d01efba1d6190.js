76e2199b0c895df29851cceb0872f8747aa8eef3
ChamodKanishka <chamodkanishka77@gmail.com>
2020-02-04 20:34:13 +0530

    add dist folder to git. its earlier ignored by gitignore. now added to git repo

---
+++ dist/precache-manifest.adaa89956e70e6ae437d01efba1d6190.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da26a8b257b63b47c8f3",
+    "url": "/css/app.523421c9.css"
+  },
+  {
+    "revision": "1ec35ca8e3358c9c0c68",
+    "url": "/css/chunk-vendors.9e653c17.css"
+  },
+  {
+    "revision": "fd42f6cbf2d662175f36d84895fdfba5",
+    "url": "/img/loading.fd42f6cb.gif"
+  },
+  {
+    "revision": "699cc67a17d9652ed0e187cc9902aebf",
+    "url": "/img/logo.699cc67a.png"
+  },
+  {
+    "revision": "e3d870d3638d9036056d920e8ea689ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da26a8b257b63b47c8f3",
+    "url": "/js/app.3814995e.js"
+  },
+  {
+    "revision": "1ec35ca8e3358c9c0c68",
+    "url": "/js/chunk-vendors.121f84e8.js"
+  },
+  {
+    "revision": "5157dd9bd249a4d478744bc8c6c3c27a",
+    "url": "/manifest.json"
+  }
+]);
