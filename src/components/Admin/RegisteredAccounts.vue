c2af0f550f9032f61d5fce83515aaa140502e5af
ChamodKanishka <chamodkanishka77@gmail.com>
2020-03-01 05:25:55 +0530

    finalize thestructure in registered accounts table

--- src/components/Admin/RegisteredAccounts.vue
+++ src/components/Admin/RegisteredAccounts.vue
@@ -82,7 +82,7 @@
                                 slot-scope="props"
                                 type="button"
                                 class="btn btn-info"
-                                value="Edit"
+                                value="View"
                                 @click="dtEditClick(props)"
                         >
                         <Spinner slot="spinner"/>
@@ -113,170 +113,191 @@
 
     const initialData = [
         {
+            id:1,
             firstName: "Lucca",
             lastName: "Lin",
-            hometown: "Melbourne",
-            dob: "13/02/1975",
+            Location: "Melbourne",
+            Contact: "13/02/1975",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:2,
             firstName: "Zahid",
             lastName: "Werner",
-            hometown: "Sydney",
-            dob: "18/09/1979",
+            Location: "Sydney",
+            Contact: "18/09/1979",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:3,
             firstName: "Gabriel",
             lastName: "Griffiths",
-            hometown: "Chicago",
-            dob: "25/11/1984",
+            Location: "Chicago",
+            Contact: "25/11/1984",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:4,
             firstName: "Talha",
             lastName: "Tucker",
-            hometown: "Berlim",
-            dob: "27/01/1999",
+            Location: "Berlim",
+            Contact: "27/01/1999",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:5,
             firstName: "Aariz",
             lastName: "Piper",
-            hometown: "Auckland",
-            dob: "11/07/1964",
+            Location: "Auckland",
+            Contact: "11/07/1964",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:6,
             firstName: "Macsen",
             lastName: "Schultz",
-            hometown: "Rio de Janeiro",
-            dob: "01/10/1987",
+            Location: "Rio de Janeiro",
+            Contact: "01/10/1987",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:7,
             firstName: "Sebastian",
             lastName: "Cervantes",
-            hometown: "Brisbane",
-            dob: "13/11/1994",
+            Location: "Brisbane",
+            Contact: "13/11/1994",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:8,
             firstName: "Tayyab",
             lastName: "Lister",
-            hometown: "Perth",
-            dob: "14/12/1997",
+            Location: "Perth",
+            Contact: "14/12/1997",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:9,
             firstName: "Anum",
             lastName: "Warren",
-            hometown: "Manaus",
-            dob: "17/02/1951",
+            Location: "Manaus",
+            Contact: "17/02/1951",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:10,
             firstName: "Areeba",
             lastName: "Stein",
-            hometown: "Rome",
-            dob: "18/03/1954",
+            Location: "Rome",
+            Contact: "18/03/1954",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:11,
             firstName: "Alesha",
             lastName: "Sharp",
-            hometown: "New York City",
-            dob: "18/04/1966",
+            Location: "New York City",
+            Contact: "18/04/1966",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:12,
             firstName: "Aadil",
             lastName: "Fitzgerald",
-            hometown: "Vancouver",
-            dob: "18/05/1967",
+            Location: "Vancouver",
+            Contact: "18/05/1967",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:13,
             firstName: "Amaya",
             lastName: "Dillon",
-            hometown: "Montreal",
-            dob: "11/06/1986",
+            Location: "Montreal",
+            Contact: "11/06/1986",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:14,
             firstName: "Hammad",
             lastName: "Ruiz",
-            hometown: "Buenos Aires",
-            dob: "12/07/1997",
+            Location: "Buenos Aires",
+            Contact: "12/07/1997",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:15,
             firstName: "Sapphire",
             lastName: "Pacheco",
-            hometown: "Mexico City",
-            dob: "12/07/1996",
+            Location: "Mexico City",
+            Contact: "12/07/1996",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:16,
             firstName: "Amanah",
             lastName: "Velez",
-            hometown: "London",
-            dob: "13/08/1992",
+            Location: "London",
+            Contact: "13/08/1992",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:17,
             firstName: "Mirza",
             lastName: "Ratliff",
-            hometown: "Manchester",
-            dob: "03/09/1981",
+            Location: "Manchester",
+            Contact: "03/09/1981",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:18,
             firstName: "Emre",
             lastName: "Amos",
-            hometown: "Sapiranga",
-            dob: "05/10/1983",
+            Location: "Sapiranga",
+            Contact: "05/10/1983",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:19,
             firstName: "Yunus",
             lastName: "Vu",
-            hometown: "Madrid",
-            dob: "05/10/1984",
+            Location: "Madrid",
+            Contact: "05/10/1984",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:20,
             firstName: "Duncan",
             lastName: "Cotton",
-            hometown: "Barcelona",
-            dob: "06/10/1985",
+            Location: "Barcelona",
+            Contact: "06/10/1985",
             created: new Date().getTime(),
             updated: new Date().getTime()
         },
         {
+            id:21,
             firstName: "Elvis",
             lastName: "Ray",
-            hometown: "Lisbon",
-            dob: "08/11/1980",
+            Location: "Lisbon",
+            Contact: "08/11/1980",
             created: new Date().getTime(),
             updated: new Date().getTime()
         }
@@ -294,7 +315,11 @@
         data: function() {
             return {
                 headerFields: [
-                    "__slot:checkboxes",
+                    {
+                        name: "id",
+                        label: "Account ID",
+                        sortable: true
+                    },
                     {
                         name: "firstName",
                         label: "First Name",
@@ -306,24 +331,24 @@
                         sortable: true
                     },
                     {
-                        name: "hometown",
-                        label: "Hometown",
+                        name: "Location",
+                        label: "Location",
                         sortable: true
                     },
                     {
-                        name: "dob",
-                        label: "Data of Birght",
+                        name: "Contact",
+                        label: "Contact No",
                         sortable: true
                     },
                     {
                         name: "created",
-                        label: "Created",
+                        label: "Account Created",
                         sortable: true,
                         format: formatDate
                     },
                     {
                         name: "updated",
-                        label: "Updated",
+                        label: "Last Updated",
                         sortable: false,
                         format: formatDate
                     },
