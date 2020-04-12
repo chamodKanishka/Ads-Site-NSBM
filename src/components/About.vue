fc8ef35a3ddfa2fde9aa4d5c5e21c543daad4abe
ChamodKanishka <chamodkanishka77@gmail.com>
2020-04-12 21:12:57 +0530

    abot page mask created

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,13 +4,6 @@
         <div class="full-width-testimonial-section">
             <div class="full-width-testimonial-icon text-center">
                 <NavHeader/>
-                <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                     width="41px" height="34px" viewBox="-235 240 41 34" style="enable-background:new -235 240 41 34;" xml:space="preserve">
-          <path class="quote-path" d="M-231.3,260.4c0-5,1.3-8.8,3.7-11.7c2.4-2.8,6-4.6,10.5-5.5v5c-3.5,1-6,2.8-7.1,5.5c-0.7,1.4-0.9,2.8-0.8,4
-            h8.1v12.8h-14.4V260.4z"/>
-                    <path class="quote-path" d="M-212,260.4c0-5,1.3-8.8,3.7-11.7c2.4-2.8,6-4.6,10.5-5.5v5c-3.5,1-6,2.8-7.1,5.5c-0.7,1.4-0.9,2.8-0.8,4h8.1
-            v12.8H-212V260.4z"/>
-      </svg>
             </div>
             <div class="full-width-testimonial-content">
                 <h5 class="full-width-testimonial-text">Lake Tahoe is the most beautiful place I have ever been to. I have traveled several places in the world, and there might be more beautiful places out there, but none that I have seen. I loved it!
@@ -71,6 +64,8 @@
 </script>
 
 <style scoped>
+
+    
     .menu{
         opacity: 1;
     }
@@ -80,7 +75,10 @@
 
     .full-width-testimonial {
         margin-top: -90px;
-        background: url("https://images.pexels.com/photos/378273/pexels-photo-378273.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb") center center no-repeat;
+        background: url("../images/nsbm.jpg") center center no-repeat;
+
+        
+
         background-size: cover;
         height: 800px;
     }
@@ -99,6 +97,7 @@
 
     .full-width-testimonial-section {
         padding-top: 2rem;
+        background-color: rgba(0,0,0,0.35);
     }
 
     .full-width-testimonial-section .full-width-testimonial-text {
@@ -168,6 +167,7 @@
 
     .full-width-testimonial-content {
         width: 100%;
+        margin-top: 40px;
     }
     ul {
         list-style-type: none;
